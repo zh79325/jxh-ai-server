bdae42d710215392c8f1d6094bc695101bd7697f
zzh250578 <zzh250578@alibaba-inc.com>
2024-11-11 22:06:33 +0800

    runtime

--- jxhaiserver/jxhaiserver/jxh_models.py
+++ jxhaiserver/jxhaiserver/jxh_models.py
@@ -6,11 +6,11 @@ dir = os.path.dirname(os.path.abspath(__file__))
 print("model dir======>" + dir)
 product_model_file = dir + "/jxh_ai/product.pt"
 print("product_model_file  exists %s => %s" % (os.path.exists(product_model_file), product_model_file))
-product_detector = {}#YOLO(product_model_file)
+product_detector = YOLO(product_model_file)
 qr_model_file = dir + "/jxh_ai/qrdet-s.pt"
 print("qr_model_file  exists %s => %s" % (os.path.exists(qr_model_file), qr_model_file))
 
-qr_detector = {}#YOLO(qr_model_file)
+qr_detector = YOLO(qr_model_file)
 
 jxh_model_info={
     'product_model_file':product_model_file,
