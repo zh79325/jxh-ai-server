ceff993b2b976305b0d2b885cab8d70b593be72e
zzh250578 <zzh250578@alibaba-inc.com>
2024-11-11 20:47:17 +0800

    detect all

--- jxh-ai-service/jxh_models.py
+++ jxh-ai-service/jxh_models.py
@@ -1,5 +1,12 @@
 from ultralytics import YOLO
 import pathlib
+import os
 
-product_detector = YOLO("./jxh-ai-service/jxh_ai/product.pt")
-qr_detector = YOLO("./jxh-ai-service/jxh_ai/qrdet-s.pt")
+dir = os.path.dirname(os.path.abspath(__file__))
+print("model dir======>" + dir)
+product_model_file = dir + "/jxh_ai/product.pt"
+print("product_model_file  exists %s => %s" % (os.path.exists(product_model_file), product_model_file))
+product_detector = YOLO(product_model_file)
+qr_model_file = dir + "/jxh_ai/qrdet-s.pt"
+print("qr_model_file  exists %s => %s" % (os.path.exists(qr_model_file), qr_model_file))
+qr_detector = YOLO(qr_model_file)
