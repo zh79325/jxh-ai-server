77f1a91812c8401d9be8a7745cca2cb46bfac9bd
zzh250578 <zzh250578@alibaba-inc.com>
2024-11-13 16:29:20 +0800

    jxh detect

--- jxh-ai-models/yolo_train.py
+++ jxh-ai-models/yolo_train.py
@@ -6,7 +6,7 @@ from build_model import datasets, model_yaml
 
 def startTrain():
     print('load model_yaml from =>',model_yaml)
-    model = YOLO('models/input/yolo11l.pt')  # build a new model from YAML
+    model = YOLO('models/input/yolo11n.pt')  # build a new model from YAML
     results = model.train(data=model_yaml, epochs=100, imgsz=1440)
 
 if __name__ == '__main__':
